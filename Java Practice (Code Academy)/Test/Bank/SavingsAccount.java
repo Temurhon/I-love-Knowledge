582e4a5942d3f06650d28fa51ac41c72edf8fe22
Temurhon Kamariddinov <temurhon.dev@gmail.com>
2018-01-15 19:17:25 +0000

    Added some comments.

--- Java Practice (Code Academy)/Test/Bank/SavingsAccount.java
+++ Java Practice (Code Academy)/Test/Bank/SavingsAccount.java
@@ -3,21 +3,29 @@ package Bank;
 public class SavingsAccount extends Account {
         private double d;
     public SavingsAccount(String id, double initialDeposit) {
-        super(id);
-         d = initialDeposit;
+        super(id);//the id object is taken from the Account class method.
+         d = initialDeposit;//the initial deposit is initialised as d.
     }
     public boolean withdraw(double amount){
 
-        if(amount > balance){
-            System.out.println("Insufficient Funds!");
-            return false;
-        }else {
-            balance = balance - withdraw(amount);
-            return true;
-        }
+        //if the account holds more than 1000 pound, then the account will get +10 pounds inside.
+
+
+        //if(amount <= 10) {
+
+    //if the amount is bigger than the balance then it will print out insuficient funds, else it will take off the amount from the withdraw amount.
+            if (amount > balance) {
+                System.out.println("Insufficient Funds!");
+                return false;
+            } else {
+                balance = balance - withdraw(amount);//the balanace will be subtracted from the withdraw amount.
+                return true;
+            }
+        //}
 
     }
     public void deposit(double amount){
-        balance += amount;
+        balance += amount;//the amount will be added to the balance.
+
     }
 }
